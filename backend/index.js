547e87b2f50ade7c3be080d59a619f5e1ab67645
ninh triệu <ninhtedam@gmail.com>
2026-06-19 11:27:46 +0700

    backend address update

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import path from "path";
 
 import authRouter from "./src/routes/auth.route.js";
 import profileRouter from "./src/routes/profile.route.js";
+import addressRouter from "./src/routes/address.route.js";
 
 const app = express();
 const SERVER_PORT = 3000;
@@ -17,6 +18,7 @@ app.use("/upload/img", express.static(uploadImageDir));
 app.use("/api/auth", authRouter);
 
 app.use("/api/profile", profileRouter);
+app.use("/api/address", addressRouter);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server is running on port ${SERVER_PORT}`);
